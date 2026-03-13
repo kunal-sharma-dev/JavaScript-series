268ab95af2b6836515c5be66c60ffbaebd24570d
kunal-sharma-dev <sharma0077kunal@gmail.com>
2026-03-13 15:04:08 +0000

    day02

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=14455
+let accountEmail="kunal@google.com"
+var accountPassword="12345"
+accountCity="delhi"
+let accountState; 
+// accountId=2 not allowed since its const
+
+accountEmail="sharma@google.com"
+accountPassword="66666"
+accountCity="babrala"
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
 console.log("kunal sharma")
+ 
